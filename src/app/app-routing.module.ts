b66e0ddb86e0cb9920f8aad85a6e677e282ff53b
truonghuusang99 <truonghuusang99@gmail.com>
2020-02-20 16:23:25 +0700

    sang

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -153,6 +153,14 @@ const routes: Routes = [
   { path: 'supplier-list', loadChildren: './pages/supplier-list/supplier-list.module#SupplierListPageModule' },
   { path: 'supplier-add', loadChildren: './pages/supplier-add/supplier-add.module#SupplierAddPageModule' },
   { path: 'supplier-detail', loadChildren: './pages/supplier-detail/supplier-detail.module#SupplierDetailPageModule' },
+  { path: 'inventory', loadChildren: './pages/inventory/inventory.module#InventoryPageModule' },
+  { path: 'sales', loadChildren: './pages/sales/sales.module#SalesPageModule' },
+  { path: 'sales-detail', loadChildren: './pages/sales-detail/sales-detail.module#SalesDetailPageModule' },
+  { path: 'sell-history', loadChildren: './pages/sell-history/sell-history.module#SellHistoryPageModule' },
+  { path: 'bill-detail', loadChildren: './pages/bill-detail/bill-detail.module#BillDetailPageModule' },
+  { path: 'sales-add', loadChildren: './pages/sales-add/sales-add.module#SalesAddPageModule' },
+  { path: 'category-product-list', loadChildren: './pages/category-product-list/category-product-list.module#CategoryProductListPageModule' },
+  { path: 'category-choose', loadChildren: './pages/category-choose/category-choose.module#CategoryChoosePageModule' }
 ];
 
 @NgModule({
